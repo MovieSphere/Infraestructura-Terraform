72a75820a24bb5982a353519163d12a7c2586897
AnjheloMarreros <wmarrerosg1@upao.edu.pe>
2025-04-29 22:57:03 -0500

    feat/Prueba para lista de películas

--- movie-recommendation-api/src/main/java/com/api_ec2/movie_recommendation_api/controller/RecommendationController.java
+++ movie-recommendation-api/src/main/java/com/api_ec2/movie_recommendation_api/controller/RecommendationController.java
@@ -1,20 +1,38 @@
 package com.api_ec2.movie_recommendation_api.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.api_ec2.movie_recommendation_api.model.Pelicula;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/recommendations")
 public class RecommendationController {
 
+    //Lista de prueba
+    private List<Pelicula> peliculasLista = Arrays.asList(
+        new Pelicula("Harry Potter", "Fantasía"),
+        new Pelicula("Sherk", "Comedia animada"),
+        new Pelicula("IT", "Terror")
+    );
+
     @GetMapping
-    public List<String> getRecommendations() {
-        //Lógica para la recomendación de películas
-        //Ejemplo de recomendación de películas
-        return Arrays.asList("Sherk 3", "El Padrino", "El Señor de los Anillos");
+    public List<String> getRecommendations(@RequestParam(value = "genre", required = false) String preferedGenre) {
+        if (preferedGenre != null && !preferedGenre.trim().isEmpty()) {
+            return peliculasLista.stream()
+                    .filter(movie -> movie.getGenre().equalsIgnoreCase(preferedGenre))
+                    .map(Pelicula::getTitle)
+                    .collect(Collectors.toList());
+        } else {
+            //El género es opcional
+            return Arrays.asList("Película(s) recomendadas: ");
+        }
+        
     }
 }
