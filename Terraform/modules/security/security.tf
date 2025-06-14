030ee88da084ef994aa56320fb73858584e04f7f
Jeffry Anderson Romero Uriol <jromerou2@upao.edu.pe>
2025-06-14 00:36:46 -0500

    [CKV_AWS_382] Ensure no security groups allow egress from 0.0.0.0:0 to port -1
    Updated egress rules to specify ports and reference security groups instead of open CIDR blocks. This ensures controlled access within the VPC, enhances security compliance, and minimizes exposure risks. Added explicit rules for HTTP, HTTPS, and PostgreSQL connections.

--- Terraform/modules/security/security.tf
+++ Terraform/modules/security/security.tf
@@ -12,11 +12,21 @@ resource "aws_security_group" "ec2_sg" {
     security_groups = [aws_security_group.alb_sg.id]
   }
 
+  # Egress solo para puertos necesarios (ej: salida a internet por proxy o NAT)
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    description = "Permite HTTP (80) y HTTPS (443)"
+    from_port   = 80
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description     = "Permite conexión a RDS PostgreSQL"
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.rds_sg.id]
   }
 
   tags = {
