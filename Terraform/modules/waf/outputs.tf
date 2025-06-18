caccce197f079200aa12cd268c5da3461a77f8b9
Jeffry Anderson Romero Uriol <jromerou2@upao.edu.pe>
2025-06-18 00:50:49 -0500

    CKV2_AWS_76 y CKV2_AWS_31

---
+++ Terraform/modules/waf/outputs.tf
@@ -0,0 +1,9 @@
+output "web_acl_arn" {
+  description = "ARN del WAF Web ACL regional"
+  value       = aws_wafv2_web_acl.main.arn
+}
+
+output "cloudfront_web_acl_arn" {
+  description = "ARN del WAF Web ACL para CloudFront"
+  value       = aws_wafv2_web_acl.cloudfront.arn
+} 
