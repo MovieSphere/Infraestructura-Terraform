055a784ca6d0d9ab08381061a725ec05252e0418
Kathia Medina Rodriguez <kmedinar1@upao.edu.pe>
2025-06-29 16:16:51 -0500

    Correccion de error [CKV2_AWS_76]

--- Terraform/environments/develop/main.tf
+++ Terraform/environments/develop/main.tf
@@ -213,3 +213,8 @@ module "acm" {
   domain_name = var.domain_name
 }
 
+resource "aws_wafv2_web_acl_association" "alb_assoc" {
+  resource_arn = module.alb.alb_arn
+  web_acl_arn  = module.waf.web_acl_arn
+}
+
