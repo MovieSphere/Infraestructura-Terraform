375faa7ee9a0296c6614bf23081fd6469629e60c
Kathia Medina Rodriguez <kmedinar1@upao.edu.pe>
2025-06-30 01:22:43 -0500

    fix error to apply

--- Terraform/modules/cloudfront/cloudfront.tf
+++ Terraform/modules/cloudfront/cloudfront.tf
@@ -254,6 +254,13 @@ resource "aws_cloudfront_distribution" "moviesphere" {
   origin {
     domain_name = "moviesphere-alb-73854958.us-east-1.elb.amazonaws.com"
     origin_id   = "moviesphereALB"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
   }
 
   enabled             = true
