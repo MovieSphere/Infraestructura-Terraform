0d393cac7763ce0075ead572024d824270af75b1
Kathia Medina Rodriguez <kmedinar1@upao.edu.pe>
2025-07-01 14:44:36 -0500

    se elimina aws_wafv2_web_acl_association

--- Terraform/modules/elb/elb.tf
+++ Terraform/modules/elb/elb.tf
@@ -24,10 +24,10 @@ resource "aws_lb" "app_alb" {
   }
 }
 
-# resource "aws_wafv2_web_acl_association" "alb_association" {
-#   resource_arn = aws_lb.app_alb.arn
-#   web_acl_arn  = var.web_acl_arn
-# }
+#  resource "aws_wafv2_web_acl_association" "alb_association" {
+#    resource_arn = aws_lb.app_alb.arn
+#    web_acl_arn  = var.web_acl_arn
+#  }
 
 resource "aws_lb_target_group" "tg_auth" {
   name     = "${var.project_name}-tg-auth"
