2f4b215ee522eda9d6cb8f8163900934d8c16f09
Kathia Medina Rodriguez <kmedinar1@upao.edu.pe>
2025-07-01 19:06:38 -0500

    Eliminar aws_eip.ec2_public_ip.public_ip de ec2/outputs.tf

--- Terraform/modules/ec2/outputs.tf
+++ Terraform/modules/ec2/outputs.tf
@@ -17,8 +17,3 @@ output "public_dns" {
   description = "DNS público de la instancia EC2"
   value       = aws_instance.ec2_ubuntu_docker.public_dns
 }
-
-output "ec2_public_ip" {
-  description = "IP pública estática asociada a la EC2"
-  value       = aws_eip.ec2_public_ip.public_ip
-}
